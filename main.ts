3076d1eafd5821a93fb898f060dd06dd0bec18cc
Tarosa <tarosa.y@gmail.com>
2021-11-08 02:20:16 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,26 @@
-let b = 0
-let ledDisp = [
-31,
-0,
-31,
-0,
-31
-]
-basic.forever(function () {
-    for (let index = 0; index < 5; index++) {
-        b = ledDisp[0]
-        if (b % 2 == 1) {
-            led.plot(0, 0)
-        } else {
-            led.unplot(0, 0)
-        }
-        b = Math.trunc(ledDisp[0] / 2)
-        if (b % 2 == 1) {
-            led.plot(0, 1)
-        } else {
-            led.unplot(0, 1)
+function LED_Disp (list: any[]) {
+    for (let x = 0; x <= 4; x++) {
+        b = ledDisp[x]
+        for (let y = 0; y <= 4; y++) {
+            if (b % 2 == 1) {
+                led.plot(x, y)
+            } else {
+                led.unplot(x, y)
+            }
+            b = Math.trunc(b / 2)
         }
     }
+}
+let b = 0
+let ledDisp: number[] = []
+ledDisp = [
+0,
+1,
+2,
+3,
+4
+]
+LED_Disp(ledDisp)
+basic.forever(function () {
+	
 })
